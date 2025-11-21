3861eeebc565f89ae298d8ade2bbd68327facb83
TimurT <ofset989@gmai.com>
2025-11-21 19:02:14 +0100

    Add playlists and add some funcions

--- car_rent.py
+++ car_rent.py
@@ -1,30 +1,14 @@
-import pandas as pd 
+import pandas as pd
+from datetime import time
+from datetime import date
 
 df_cars = pd.read_csv("cars.csv", dtype={"id": str})
+df_playlists = pd.read_csv("playlist.csv", dtype={"id": str})
 
-def main():
-    print(df_cars)
-    init_of_rent= input("If yo want to choose a car to rent pres enter")
-    if init_of_rent:
-        car_id= input("Please input the cars id:")
-        car= Cars (id = car_id)
-        if car.avaible:
-            car.booking
-
-        else:
-            print("Thiss car is not avaible or not existing")
-    
-    else:
-        print("Thank you fo visiting our web")
-
-
-class Cars():
+class Car():
     def __init__ (self, id):
         self.id = id
     
-    def booking(self):
-        df_cars.loc[df_cars["id"] == self.id, "available"] = "no"
-        df_cars.to_csv("cars.csv", index=False)
 
     def avaible (self):
         available = df_cars.loc[df_cars["id"] == self.id, "available"].squeeze()
@@ -33,8 +17,60 @@ class Cars():
         else:
             return False
         
-class Reservation_Ticket():
-    def generate(self):
-        pass
+    def booking(self):
+        df_cars.loc[df_cars["id"] == self.id, "available"] = "No"
+        df_cars.to_csv("cars.csv", index=False)
         
+class PlaylistService ():
+    def __init__(self, playlist_name, ):
+        self.name = playlist_name
+    
+    def output_playlist (self):
+        self.link = df_playlists.loc[df_playlists["name"] == self.name, "Link"].squeeze()
+        
+
+class Reservation_Ticket(PlaylistService):
+    def __init__(self, name,model ):
+        self.name = name
+        self.model = model
+
+    
+    def generate(self):
+        print (f"""
+                Your car model is: {self.model}
+                Your customer name is: {self.name}
+                The date of the booking is: {date}
+                Time of the booking is: {time}
+                Your playlist link is: {self.link}
+ """)
+        
+
+
+def main():
+    print(df_cars)
+    init_of_rent= input("If yo want to choose a car to rent pres enter")
+    if init_of_rent:
+        car_id= input("Please input the cars id:")
+        car= Car (id = car_id)
+        if car.avaible():
+            car.booking()
+            customer_name = input ("Please input your name:")
+            car_model = df_cars.loc[df_cars["id"] == car_id, "model"].squeeze()
+            init_of_playlist = input ("If you want to choose playlis for your trip press enter")
+            if init_of_playlist:
+                print (df_playlists)
+                playlist_name = input ("Please input the playlist name:")
+                playlist = PlaylistService (playlist_name)
+            else:
+                pass
+            rent_confirmation = Reservation_Ticket(customer_name, car_model)
+            rent_confirmation.generate
+            
+
+        else:
+            print("Thiss car is not avaible or not existing")
+    
+    else:
+        print("Thank you fo visiting our web")
+
         
